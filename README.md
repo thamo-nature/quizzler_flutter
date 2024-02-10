cc46f4e5ed78cd352358ccb68dcbfd30cb4c08df
thamothiran <thamothiran.s@spovum.com>
2024-02-11 05:03:45 +0530

    [develope] - Finished the app with dart oops

--- README.md
+++ README.md
@@ -1,2 +1,134 @@
 # quizzler_flutter
 Quiz app
+
+<h5>Children Widget as a list</h5>
+
+<pre>
+              const Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  ),
+                  Icon(
+                    Icons.close,
+                    color: Colors.red,
+                  )
+                ],
+              )
+
+                List<String> answersScore = [
+                    Icon(
+                        Icons.check,
+                        color: Colors.green,
+                    ),
+                    Icon(
+                        Icons.close,
+                        color: Colors.red,
+                    )
+                  ];
+
+              const Row(
+                children: answersScore,
+              )
+</pre>
+
+<h5>Question list</h5>
+<pre>
+
+  final List<String> questionList = [
+    'Some cats are actually allergic to humans',
+    'You can lead a cow down stairs but not up stairs.'
+  ];
+
+  int questionnumber = 0;
+
+  questionList[questionnumber],
+
+    setState(() {
+        questionnumber++;
+    });
+</pre>
+
+<h5>Question class</h5>
+<pre>
+
+class Question {
+  String questionText = "";
+  bool questionAnswers = true;
+
+  Question({String q = "", bool a = true}) {
+    questionText = q;
+    questionAnswers = a;
+  }
+}
+
+
+  final List<Question> questionBook = [
+    Question(q: 'Some cats are actually allergic to humans', a: true),
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+  ];
+
+Question string to display becomes
+
+    questionBook[questionnumber].questionText,
+
+Checking Answer becomes
+
+    bool checkAnswer = questionBook[questionnumber].questionAnswers;
+
+</pre>
+
+<h5>Abstraction of Questions in QuizBrain class</h5>
+<pre>
+
+import 'question.dart';
+
+class QuizBrain {
+  final List<Question> questionBook = [
+    Question('Some cats are actually allergic to humans', true),
+  ];
+}
+
+usage in main dart as:
+
+    import 'quiz_brain.dart';
+
+    final newQuestion = QuizBrain();s
+
+Question text becomes:
+
+      newQuestion.questionBook[questionnumber].questionText,
+
+Question answers becomes:
+
+         bool checkAnswer = newQuestion
+                              .questionBook[questionnumber].questionAnswers;
+</pre>
+
+<h5>OOPS in class</h5>
+<pre>
+
+  Encapsulation:
+    Private variables little sign _ 
+
+      final List<Question> _questionBook = [
+        Question('Some cats are actually allergic to humans', true)
+      ]; 
+
+  inherit :
+
+    class alien extends human{ void emotion(){ print('crying') } }
+
+  Polymorphism: inherit similarities and override 
+
+    class alien extends human{
+      @override
+      void emotion(){
+        super.emotion();
+        //plus additional characteristics not common in human
+
+        print('crying with no tears');
+      }
+    }
+</pre>
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,124 @@
 import 'package:flutter/material.dart';
+//import 'question.dart';
 
-void main() {
-  runApp(const MainApp());
+import 'quiz_brain.dart';
+
+void main() => runApp(const QuizzApp());
+
+final newQuestion = QuizBrain();
+
+class QuizzApp extends StatefulWidget {
+  const QuizzApp({super.key});
+
+  @override
+  State<QuizzApp> createState() => _QuizzAppState();
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class _QuizzAppState extends State<QuizzApp> {
+  final List<Icon> answersScore = [
+    // const Icon(
+    //   Icons.check,
+    //   color: Colors.green,
+    // ),
+  ];
+
+  void checkAnswersQuestions() {
+    bool checkAnswer = newQuestion.loadQuestionAnswer(questionnumber);
+
+    setState(() {
+      questionnumber++;
+      if (checkAnswer == true) {
+        answersScore.add(
+          const Icon(
+            Icons.check,
+            color: Colors.green,
+          ),
+        );
+      } else {
+        answersScore.add(
+          const Icon(
+            Icons.close,
+            color: Colors.red,
+          ),
+        );
+      }
+    });
+  }
+
+  // final List<String> questionList = [
+  //   'Some cats are actually allergic to humans',
+  //   'You can lead a cow down stairs but not up stairs.'
+  // ];
+
+  // final List<bool> answers = [true, false];
+
+  int questionnumber = 0;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Center(
+                  child: Text(
+                    //newQuestion.questionBook[questionnumber].questionText,
+                    newQuestion.loadQuestionText(questionnumber),
+
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Center(
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          fixedSize: const Size(300, 70.0),
+                          backgroundColor: Colors.green,
+                        ),
+                        onPressed: () {
+                          checkAnswersQuestions();
+                        },
+                        child: const Text(
+                          'True',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Center(
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          fixedSize: const Size(300, 70.0),
+                          backgroundColor: Colors.red,
+                        ),
+                        onPressed: () {
+                          checkAnswersQuestions();
+                        },
+                        child: const Text(
+                          'False',
+                          style: TextStyle(
+                            color: Colors.white,
+                            //backgroundColor: Colors.amber,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                children: answersScore,
+              ),
+            ],
+          ),
         ),
       ),
     );
